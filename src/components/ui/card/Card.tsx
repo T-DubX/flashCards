650dd70d8bc30bff9c5d2a10adc9a5d036d84ecc
Anton <spone161@gmail.com>
2024-03-07 21:02:42 +0300

    fix: сhange the Card component to be polymorphic

--- src/components/ui/card/Card.tsx
+++ src/components/ui/card/Card.tsx
@@ -3,17 +3,16 @@ import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
 import s from './card.module.scss'
 
 export type Props<T extends ElementType = 'div'> = {
+  as: T
   children: ReactNode
-  title?: string
 } & ComponentPropsWithoutRef<T>
 
 export const Card = (props: Props) => {
-  const { children, title } = props
+  const { as: Component = 'div', children, className, title, ...rest } = props
 
   return (
-    <div className={s.wrapperCard}>
-      <div className={s.cardTitle}>{title}</div>
+    <Component className={`${s.wrapperCard} ${className ? className : ''}`} {...rest}>
       <div className={s.content}>{children}</div>
-    </div>
+    </Component>
   )
 }
