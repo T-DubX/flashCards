fbac72a80f475812feefa321bb5394cc968961c0
Anton <spone161@gmail.com>
2024-03-11 19:49:36 +0300

    fix: add forwardRef in RadioGroup

--- src/components/ui/radioGroup/RadioGroup.tsx
+++ src/components/ui/radioGroup/RadioGroup.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef } from 'react'
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 
 import s from './radioGroup.module.scss'
 
@@ -15,28 +15,28 @@ export type Props = {
   options: Option[]
 } & ComponentPropsWithoutRef<typeof RadioGroupRadix.Root>
 
-export const RadioGroup = (props: Props) => {
-  const { disabledAll, options, ...rest } = props
-
-  return (
-    <div>
-      <RadioGroupRadix.Root className={s.radioRoot} disabled={disabledAll} {...rest}>
-        {options.map((option, idx) => (
-          <div className={s.wrapperBox} key={idx}>
-            <RadioGroupRadix.Item
-              className={s.radioItem}
-              disabled={option.disabled}
-              id={option.value}
-              value={option.value}
-            >
-              <RadioGroupRadix.Indicator className={s.radioIndicator} />
-            </RadioGroupRadix.Item>
-            <Typography as={'label'} className={s.label} htmlFor={option.value} variant={'body2'}>
-              {option.label}
-            </Typography>
-          </div>
-        ))}
-      </RadioGroupRadix.Root>
-    </div>
-  )
-}
+export const RadioGroup = forwardRef<ElementRef<typeof RadioGroupRadix.Root>, Props>(
+  ({ disabledAll, options, ...rest }, ref) => {
+    return (
+      <div>
+        <RadioGroupRadix.Root className={s.radioRoot} disabled={disabledAll} ref={ref} {...rest}>
+          {options.map((option, idx) => (
+            <div className={s.wrapperBox} key={idx}>
+              <RadioGroupRadix.Item
+                className={s.radioItem}
+                disabled={option.disabled}
+                id={option.value}
+                value={option.value}
+              >
+                <RadioGroupRadix.Indicator className={s.radioIndicator} />
+              </RadioGroupRadix.Item>
+              <Typography as={'label'} className={s.label} htmlFor={option.value} variant={'body2'}>
+                {option.label}
+              </Typography>
+            </div>
+          ))}
+        </RadioGroupRadix.Root>
+      </div>
+    )
+  }
+)
