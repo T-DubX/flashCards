ff0565253c09738bbb7d809089c48c00a6afe6fd
Anton <spone161@gmail.com>
2024-03-11 19:00:34 +0300

    feat: add forwardRef in CheckBox

--- src/components/ui/checkbox/Checkbox.tsx
+++ src/components/ui/checkbox/Checkbox.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef } from 'react'
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 
 import * as CheckboxRadix from '@radix-ui/react-checkbox'
 
@@ -7,24 +7,22 @@ import s from './checkbox.module.scss'
 import { CheckIcon, Typography } from './index'
 
 export type CheckboxProps = {
-  disabled?: boolean
-  id: string
   label?: string
 } & ComponentPropsWithoutRef<typeof CheckboxRadix.Root>
 
-export const Checkbox = (props: CheckboxProps) => {
-  const { disabled, id, label, ...rest } = props
-
-  return (
-    <div className={s.wrapper}>
-      <CheckboxRadix.Root className={s.checkbox} disabled={disabled} id={id} {...rest}>
-        <CheckboxRadix.Indicator className={s.check}>
-          <CheckIcon />
-        </CheckboxRadix.Indicator>
-      </CheckboxRadix.Root>
-      <Typography as={'label'} htmlFor={id} variant={'body2'}>
-        {label}
-      </Typography>
-    </div>
-  )
-}
+export const Checkbox = forwardRef<ElementRef<typeof CheckboxRadix.Root>, CheckboxProps>(
+  ({ disabled, id, label, ...rest }, ref) => {
+    return (
+      <div className={s.wrapper}>
+        <CheckboxRadix.Root className={s.checkbox} disabled={disabled} id={id} ref={ref} {...rest}>
+          <CheckboxRadix.Indicator className={s.check}>
+            <CheckIcon />
+          </CheckboxRadix.Indicator>
+        </CheckboxRadix.Root>
+        <Typography as={'label'} htmlFor={id} variant={'body2'}>
+          {label}
+        </Typography>
+      </div>
+    )
+  }
+)
