55fa7445e781104f664ee4a1bbef92ddd3ab7aa0
Anton <spone161@gmail.com>
2024-03-25 22:46:35 +0300

    feat: add Header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,7 +1,35 @@
+import { Link } from 'react-router-dom'
+
+import { Logo } from '@/assets/icon/Logo'
+import { HeaderDropDown } from '@/components/layout/header/headerDropDown'
+import { Button } from '@/components/ui/button'
+
 import s from './header.module.scss'
 
-type Props = {}
+export type ProfileData = {
+  avatar: string
+  email: string
+  name: string
+}
 
-export const Header = ({}: Props) => {
-  return <header className={s.header}></header>
+type Props = {
+  isAuth: boolean
+  logout: () => void
+  profile: ProfileData
+}
+
+export const Header = ({ isAuth, logout, profile }: Props) => {
+  return (
+    <header className={s.header}>
+      <Link to={'/'}>
+        <Logo />
+      </Link>
+
+      {isAuth ? (
+        <HeaderDropDown logout={logout} profileData={profile} />
+      ) : (
+        <Button variant={'secondary'}>Sign In</Button>
+      )}
+    </header>
+  )
 }
