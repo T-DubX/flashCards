4076a04959387111d4a49aed6dbe5ea61b417889
Anton <spone161@gmail.com>
2024-04-01 14:27:12 +0300

    fix: change Checkbox with className

--- src/components/ui/checkbox/Checkbox.tsx
+++ src/components/ui/checkbox/Checkbox.tsx
@@ -1,20 +1,28 @@
 import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 
 import * as CheckboxRadix from '@radix-ui/react-checkbox'
+import clsx from 'clsx'
 
 import s from './checkbox.module.scss'
 
 import { CheckIcon, Typography } from './index'
 
 export type CheckboxProps = {
+  className?: string
   label?: string
 } & ComponentPropsWithoutRef<typeof CheckboxRadix.Root>
 
 export const Checkbox = forwardRef<ElementRef<typeof CheckboxRadix.Root>, CheckboxProps>(
-  ({ disabled, id, label, ...rest }, ref) => {
+  ({ className, disabled, id, label, ...rest }, ref) => {
     return (
       <div className={s.wrapper}>
-        <CheckboxRadix.Root className={s.checkbox} disabled={disabled} id={id} ref={ref} {...rest}>
+        <CheckboxRadix.Root
+          className={clsx(s.checkbox, className)}
+          disabled={disabled}
+          id={id}
+          ref={ref}
+          {...rest}
+        >
           <CheckboxRadix.Indicator className={s.check}>
             <CheckIcon />
           </CheckboxRadix.Indicator>
