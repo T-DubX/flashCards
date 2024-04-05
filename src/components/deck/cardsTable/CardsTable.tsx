4a1f1be4ee7ebe72e69a246f8eba7b73094d37dd
Anton <spone161@gmail.com>
2024-04-05 15:40:29 +0300

    feat: add cardsTable

---
+++ src/components/deck/cardsTable/CardsTable.tsx
@@ -0,0 +1,73 @@
+import { EditTwoOutline } from '@/assets/icon/EditTwoOutline'
+import { Trash } from '@/assets/icon/Trash'
+import { dateTimeFormat } from '@/common/utils'
+import { Column, TableSortHeader } from '@/components/tableSortHeader'
+import { Button } from '@/components/ui/button'
+import { Rating } from '@/components/ui/rating'
+import { Table } from '@/components/ui/table/Table'
+import { Typography } from '@/components/ui/typography'
+import { CardsItems } from '@/services/deck'
+
+import s from './cardsTable.module.scss'
+
+type Props = {
+  cards: CardsItems[] | undefined
+  isOwner: boolean
+}
+
+const columns: Column[] = [
+  { cols: '3', key: 'question', title: 'Question' },
+  { cols: '3', key: 'answer', title: 'Answer' },
+  { cols: '2', key: 'update', title: 'Last Update' },
+  { cols: '2', key: 'grade', title: 'Grade' },
+]
+
+export const CardsTable = ({ cards, isOwner }: Props) => {
+  return (
+    <Table.Root>
+      <TableSortHeader columns={columns} />
+      <Table.Body>
+        {cards?.map(card => (
+          <Table.Row key={card.id}>
+            <Table.Cell col={'3'}>
+              <div className={s.questionCell}>
+                {card.questionImg && (
+                  <img alt={'Question img'} className={s.img} src={card.questionImg} />
+                )}
+                <Typography title={card.question} variant={'body2'}>
+                  {card.question}
+                </Typography>
+              </div>
+            </Table.Cell>
+            <Table.Cell col={'3'}>
+              <div className={s.answerCell}>
+                {card.answerImg && (
+                  <img alt={'Answer img'} className={s.img} src={card.answerImg} />
+                )}
+                <Typography title={card.answer} variant={'body2'}>
+                  {card.answer}
+                </Typography>
+              </div>
+            </Table.Cell>
+            <Table.Cell col={'2'}>
+              <Typography variant={'body2'}>{dateTimeFormat(card.updated)}</Typography>
+            </Table.Cell>
+            <Table.Cell col={'2'}>
+              <Rating rating={card.grade} />
+              {isOwner && (
+                <div>
+                  <Button variant={'icon'}>
+                    <EditTwoOutline />
+                  </Button>
+                  <Button variant={'icon'}>
+                    <Trash />
+                  </Button>
+                </div>
+              )}
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table.Root>
+  )
+}
