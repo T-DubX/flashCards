e9baf52d03280dd2ec787c3bdea6cd263b21ea95
Anton <spone161@gmail.com>
2024-04-21 18:24:47 +0300

    fix: remove unused properties from the store

--- src/services/decks/decks.selector.ts
+++ src/services/decks/decks.selector.ts
@@ -2,7 +2,5 @@ import { RootState } from '@/services/store'
 
 export const selectDecksCurrentTab = (state: RootState) => state.decks.currentTab
 export const selectDecksCurrentPage = (state: RootState) => state.decks.currentPage
-export const selectDecksMinCards = (state: RootState) => state.decks.minCards
-export const selectDecksMaxCards = (state: RootState) => state.decks.maxCards
 export const selectDecksPageSize = (state: RootState) => state.decks.pageSize
 export const selectDecksOrderBy = (state: RootState) => state.decks.orderBy
