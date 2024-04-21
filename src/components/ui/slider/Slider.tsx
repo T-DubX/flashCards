a1dbc288d53d6fdf69cbc358e17dd41cf2ca1a0c
Anton <spone161@gmail.com>
2024-04-21 18:14:57 +0300

    fix: add the ability to pass slider props from radix to slider props

--- src/components/ui/slider/Slider.tsx
+++ src/components/ui/slider/Slider.tsx
@@ -1,3 +1,5 @@
+import { ComponentPropsWithoutRef } from 'react'
+
 import * as SliderRadix from '@radix-ui/react-slider'
 
 import s from './slider.module.scss'
@@ -9,10 +11,10 @@ export type Props = {
   min?: number | undefined
   onValueChange: (value: number[]) => void
   value: number[]
-}
+} & ComponentPropsWithoutRef<typeof SliderRadix.Root>
 
 export const Slider = (props: Props) => {
-  const { max = 10, min, onValueChange, value } = props
+  const { max = 10, min, onValueChange, value, ...rest } = props
 
   return (
     <div className={s.wrapperSlider}>
@@ -27,6 +29,7 @@ export const Slider = (props: Props) => {
         onValueChange={onValueChange}
         step={1}
         value={value}
+        {...rest}
       >
         <SliderRadix.Track className={s.sliderTrack}>
           <SliderRadix.Range className={s.sliderRange} />
